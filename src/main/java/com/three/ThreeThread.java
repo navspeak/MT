cf7134fbd54226155b42accf8543b054544300e6
navspeak <navneet.sahay@gmail.com>
2018-09-09 10:54:10 +0530

    Three Thread v2 - Executors Framework

--- src/main/java/com/three/ThreeThread.java
+++ src/main/java/com/three/ThreeThread.java
@@ -27,10 +27,36 @@ public class ThreeThread {
         }
     };
 
+    static Runnable PrintNumbersTask_waitnotify = () -> {
+         while (true) {
+            synchronized (lock) {
+                if (val >=12) return;
+                final String threadName = Thread.currentThread().getName();
+                // Thread name is of format "pool-1-Thread-1 for Executors"
+                int threadNo = Integer.parseInt(threadName.substring(threadName.length()-1)) - 1;
+                // Thread-1 is ThreadNo 0 => It will print 1,4,7...
+                // Thread-2 is ThreadNo 1 => It will print 2,5,9...
+                // Thread-3 is ThreadNo 2 => It will print 3,6,10...
+                while (val % NUM_THREADS != threadNo ) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {/*ignore*/}
+
+                }
+                System.out.println(threadName + " is printing " + ++val);
+                lock.notifyAll();// If I have only Notify, I get a deadlock
+                // because notify may chose to wake wrong thread all the time
+                // correct thread may keep on waiting
+            }
+        }
+    };
+
+
+
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
         for (int i = 0; i < NUM_THREADS; i++) {
-            executorService.submit(PrintNumbersTask);
+            executorService.submit(PrintNumbersTask_waitnotify);
         }
         executorService.shutdown();
         executorService.awaitTermination(1, TimeUnit.HOURS);
